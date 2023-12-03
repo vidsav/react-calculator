797cc7e81c0f99939a13c3c7a7ee4b3e8e6611b4
Vid <savnikvid5@gmail.com>
2023-12-03 16:25:53 +0100

    onClick and useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,27 @@ function App() {
     const numbersButtons = [0,1,2,3,4,5,6,7,8,9];
     const operatorsButtons = ['+','-','*','/','='];
 
+    const [calculationView, setCalculationView] = useState(null);
+
+    const buttonsClickHandler = (action) => {
+        console.log(action);
+        setCalculationView(action);
+    }
+
     return (
         <>
-            <div>
-                {numbersButtons.map(number=>{
-                    return <button key={number}>{number}</button>
-                })}
-                {operatorsButtons.map(operator=>{
-                    return <button key={operator}>{operator}</button>
-                })}
+            <div>{calculationView}</div>
+            <div className="flex">
+                <div className="grid gap-4 grid-cols-3">
+                    {numbersButtons.map(number=>{
+                        return <button onClick={()=>{buttonsClickHandler(number)}} className="p-5 border border-indigo-600" key={number}>{number}</button>
+                    })}
+                </div>
+                <div className="grid gap-4 grid-cols-1">
+                    {operatorsButtons.map(operator=>{
+                        return <button onClick={()=>{buttonsClickHandler(operator)}} className="p-5 border border-indigo-600" key={operator}>{operator}</button>
+                    })}
+                </div>
             </div>
         </>
     )

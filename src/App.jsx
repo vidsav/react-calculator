46c7a4fcf3a4fbdf3e31ef3310e17af02dc00773
Vid <savnikvid5@gmail.com>
2023-12-03 16:10:04 +0100

    cleared out vite template, added initial arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const numbersButtons = [0,1,2,3,4,5,6,7,8,9];
+    const operatorsButtons = ['+','-','*','/','='];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                {numbersButtons.map(number=>{
+                    return <button key={number}>{number}</button>
+                })}
+                {operatorsButtons.map(operator=>{
+                    return <button key={operator}>{operator}</button>
+                })}
+            </div>
+        </>
+    )
 }
 
 export default App
